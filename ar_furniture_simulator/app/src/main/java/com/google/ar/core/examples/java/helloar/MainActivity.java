57a1144b59763d86574a7b9d5746f6e2527af8ef
허진석 <gwt1505@gmail.com>
2022-11-21 23:55:21 +0900

    recently coded

---
+++ ar_furniture_simulator/app/src/main/java/com/google/ar/core/examples/java/helloar/MainActivity.java
@@ -0,0 +1,54 @@
+package com.google.ar.core.examples.java.helloar;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+
+
+        //개발자 정보 버튼 클릭시 액티비티 전환
+        Button button = (Button) findViewById(R.id.wdh_who);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),OpenGLES20Activity.class);
+                EditText editPhone1 = (EditText) findViewById(R.id.ww);
+                EditText editPhone2 = (EditText) findViewById(R.id.dd);
+                EditText editPhone3 = (EditText) findViewById(R.id.hh);
+                String width = editPhone1.getText().toString();
+                String depth = editPhone2.getText().toString();
+                String height = editPhone3.getText().toString();
+                Bundle bd = new Bundle();
+                bd.putString("width",width);
+                bd.putString("depth",depth);
+                bd.putString("height",height);
+                intent.putExtras(bd);
+                startActivity(intent);
+            }
+        });
+        Button button1 = (Button) findViewById(R.id.argogo);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(getApplicationContext(), HelloArActivity.class);
+                startActivity(intent1);
+            }
+        });
+
+
+    }
+}
